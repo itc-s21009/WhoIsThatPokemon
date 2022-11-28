0b661198430d9d378fc6a8bcffb049faa13083d9
itc-s21009 <s21009@std.it-college.ac.jp>
2022-11-28 10:52:31 +0900

    json読み込み関数を別ファイルに分けた

--- app/src/main/java/jp/ac/it_college/std/s21009/whoisthatpokemon/JsonLoader.kt
+++ app/src/main/java/jp/ac/it_college/std/s21009/whoisthatpokemon/JsonLoader.kt
@@ -1,6 +1,13 @@
 package jp.ac.it_college.std.s21009.whoisthatpokemon
 
-class JsonLoader {
+import android.content.res.AssetManager
+import com.google.gson.Gson
+import java.io.InputStreamReader
+import kotlin.reflect.KClass
 
-
+fun <T : Any> parseJson(assets: AssetManager, file: String, c: KClass<T>): T {
+    val inputStream = assets.open(file)
+    val jsonReader = InputStreamReader(inputStream, "UTF-8").readText()
+    inputStream.close()
+    return Gson().fromJson(jsonReader, c.java)
 }
--- app/src/main/java/jp/ac/it_college/std/s21009/whoisthatpokemon/SelectGenerationActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21009/whoisthatpokemon/SelectGenerationActivity.kt
@@ -1,26 +1,11 @@
 package jp.ac.it_college.std.s21009.whoisthatpokemon
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
-import android.util.Log
-import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams
-import android.view.ViewGroup.MarginLayoutParams
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TableRow
-import com.google.gson.Gson
+import androidx.appcompat.app.AppCompatActivity
 import jp.ac.it_college.std.s21009.whoisthatpokemon.databinding.ActivitySelectGenerationBinding
-import jp.ac.it_college.std.s21009.whoisthatpokemon.model.pokedex.Pokedex
 import jp.ac.it_college.std.s21009.whoisthatpokemon.model.pokedex.PokedexJson
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import kotlin.reflect.KClass
 
 class SelectGenerationActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySelectGenerationBinding
@@ -31,7 +16,7 @@ class SelectGenerationActivity : AppCompatActivity() {
         title = getString(R.string.select_generation)
 
         setContentView(binding.root)
-        parseJson("filtered_pokedex.json", PokedexJson::class).pokedex.map { it.name }
+        parseJson(assets, "filtered_pokedex.json", PokedexJson::class).pokedex.map { it.name }
             .forEach { name ->
                 val button = Button(this).apply { text = name }
                 button.setOnClickListener {
@@ -41,12 +26,5 @@ class SelectGenerationActivity : AppCompatActivity() {
                 }
                 binding.generations.addView(button)
             }
-    }
-
-    private fun <T : Any> parseJson(file: String, c: KClass<T>): T {
-        val inputStream = assets.open(file)
-        val jsonReader = InputStreamReader(inputStream, "UTF-8").readText()
-        inputStream.close()
-        return Gson().fromJson(jsonReader, c.java)
     }
 }
